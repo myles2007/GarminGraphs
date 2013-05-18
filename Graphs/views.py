b1e3dc6b8d9b6e088207142aa6eb651c4d52c3d5
Myles Loffler <myles2007@britecore.com>
2013-05-18 12:14:59 -0500

    Uploads sort of work...

--- Graphs/views.py
+++ Graphs/views.py
@@ -1,6 +1,24 @@
+import os
+
 from Graphs import app
 import flask
+import werkzeug
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if flask.request.method == 'POST':
+        import pudb; pudb.set_trace()
+        file_ = flask.request.files['file']
+        if file_ and allowed_file(file_.filename):
+            filename = werkzeug.secure_filename(file_.filename)
+            file_.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return '<div> Success! </div>'
+    return '<div> ??? </div'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
